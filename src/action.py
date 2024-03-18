8e6e6a8f866d1f584572cca2c0324a0eacddfc8b
Zhi Yan Liu <liuzhiya@amazon.com>
2024-03-18 15:01:50 +0800

    Update code

--- src/action.py
+++ src/action.py
@@ -1,13 +1,11 @@
-# Git functionallity for Python. Included in src/requirements.txt.
-from jk_git import *
-# GitHub functionallity for Python. Included in src/requirements.txt. Fore more information, please see:
-# https://github.blog/2020-12-18-learn-about-ghapi-a-new-third-party-python-client-for-the-github-api/
-from ghapi.core import GhApi
-GH = GhApi()
-                              # Start of Action #
-# A simple "Hello, World!" action.
-#
-# Replace the entire content of this file with your own content, except the first 6 lines.
-# After replacing the code, that code will be executed when the Actions workflow
-# starts this action.
-print("Hello World!")
+import os
+
+try:
+    from jk_git import *
+except:
+    print("jk_git not available")
+else:
+    print("jk_git available")
+
+github_output_env = os.environ.get('GITHUB_OUTPUT', "NOT-SET")
+print("Hello World! {}".format(github_output_env))
