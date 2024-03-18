41bbf3caf3ac752ac85dd536057310a446417f88
Zhi Yan Liu <liuzhiya@amazon.com>
2024-03-18 15:21:54 +0800

    Update code

--- src/action.py
+++ src/action.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import os
 
 try:
@@ -9,7 +10,7 @@ else:
 
 # Receive the inputs
 who_to_greet = core.get_input('who-to-greet', required=True, trim_whitespace=True)
-print("hi {}! Hello World to you!".format(who_to_greet))
+print("Hello {}!".format(who_to_greet))
 
 
 github_output_env = os.environ.get('GITHUB_OUTPUT', "NOT-SET")
@@ -18,4 +19,4 @@ github_input_env = os.environ.get('INPUT_WHO-TO-GREET', "NOT-SET")
 print("Env GITHUB_OUTPUT: {}\nINPUT_WHO-TO-GREET: {}".format(github_output_env, github_input_env))
 
 # Return the outputs
-core.set_output('my_response', 'You too!')
+core.set_output('time', datetime.now().strftime("%A, %B %d, %Y %I:%M:%S %p"))
