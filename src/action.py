2486f921df6dff5468d6b9c8ccffeab0b0628a1a
Zhi Yan Liu <liuzhiya@amazon.com>
2024-03-18 15:21:54 +0800

    Update code

--- src/action.py
+++ src/action.py
@@ -1,5 +1,6 @@
-from datetime import datetime
 import os
+import pathlib
+from datetime import datetime
 
 try:
     from actions_toolkit import core
@@ -8,10 +9,20 @@ except:
 else:
     print("core available")
 
+
+def set_output(key: str, value: str):
+    file_path = os.getenv("GITHUB_OUTPUT")
+    if not file_path:
+        raise Exception(f"Unable to find output file GITHUB_OUTPUT")
+    if not pathlib.Path(file_path).exists():
+        raise Exception(f"Missing file at path: {file_path}")
+    with open(file_path, "a", encoding="utf8", newline="") as f:
+        f.write(f"{key.upper()}={value}{os.linesep}")
+
+
 # Receive the inputs
 who_to_greet = core.get_input('who-to-greet', required=True)
 print("Hello {}!".format(who_to_greet))
-
 
 github_output_env = os.environ.get('GITHUB_OUTPUT', "NOT-SET")
 github_input_env = os.environ.get('INPUT_WHO-TO-GREET', "NOT-SET")
@@ -19,4 +30,4 @@ github_input_env = os.environ.get('INPUT_WHO-TO-GREET', "NOT-SET")
 print("Env GITHUB_OUTPUT: {}\nINPUT_WHO-TO-GREET: {}".format(github_output_env, github_input_env))
 
 # Return the outputs
-core.set_output('time', datetime.now().strftime("%A, %B %d, %Y %I:%M:%S %p"))
+set_output('time', datetime.now().strftime("%A, %B %d, %Y %I:%M:%S %p"))
