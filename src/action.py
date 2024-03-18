0331613cd4a1fca5897e76020e71af6ba2be53d8
Zhi Yan Liu <liuzhiya@amazon.com>
2024-03-18 15:21:54 +0800

    Update code

--- src/action.py
+++ src/action.py
@@ -7,10 +7,15 @@ except:
 else:
     print("core available")
 
-who_to_greet = core.get_input('who-to-greet', required=True)
+# Receive the inputs
+who_to_greet = core.get_input('who-to-greet', required=True, trim_whitespace=True)
 print("hi {}! Hello World to you!".format(who_to_greet))
 
-github_output_env = os.environ.get('GITHUB_OUTPUT', "NOT-SET")
-github_input_env = os.environ.get('INPUT_WHO_TO_GREET', "NOT-SET")
 
-print("Env GITHUB_OUTPUT: {}\nINPUT_WHO_TO_GREET: {}".format(github_output_env, github_input_env))
+github_output_env = os.environ.get('GITHUB_OUTPUT', "NOT-SET")
+github_input_env = os.environ.get('INPUT_WHO-TO-GREET', "NOT-SET")
+
+print("Env GITHUB_OUTPUT: {}\nINPUT_WHO-TO-GREET: {}".format(github_output_env, github_input_env))
+
+# Return the outputs
+core.set_output('my_response', 'You too!')
